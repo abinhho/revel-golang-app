f2d23c3dc7f274edf6f8cdab47cfd31bb8deb6cc
Binh <binhht@necsv-hcm.com>
2019-08-20 12:11:03 +0700

    Add bookModel

---
+++ app/models/book_model.go
@@ -0,0 +1,32 @@
+package models
+
+import {
+	"error",
+	"regexp",
+	"gihub.com/revel/revel"
+}
+
+type BookModel struct {
+	ID int `json: "id"`
+	BookName string `json:bookname`
+	BookCatId int `json:bookcatid`
+	Created int64 `json:-`
+	Updated int64 `json:-`
+}
+
+func (book *BookModel) Validate() error {
+	var v revel.Validation
+
+	v.check(
+		book.BookName,
+		revel.required{},
+		revel.MinSize{4}
+		revel.MaxSize{255},
+	)
+
+	if v.HasError() {
+		return errors.New("Book name is not valid")
+	}
+
+	return nil
+}
